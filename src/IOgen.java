1ff8a448143736efa181d392a1597a715efb7e49
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-08-04 22:13:30 +0530

    Updated IO generator interface

---
+++ src/IOgen.java
@@ -0,0 +1,15 @@
+public class IOgen {
+
+    // Operation type
+    int op = 0; // 0 - No op, 1 - create, 2 - read/write, 3 - delete
+
+    // File size for create op
+    int numBlocks = 0;
+
+    // Random file index for ops 2 and 3
+    int fileIndex = 0;
+
+    // Operation iteration counter
+    int OIC;
+
+}
