05d69af5ec4fabe232df1dda27ea3448f34e1565
udit01 <uditjainiitd@gmail.com>
2018-08-07 10:54:28 +0530

    Block, IO time update remaining

--- src/Block.java
+++ src/Block.java
@@ -1,7 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
 
-public interface Block {
+public class Block {
+
+    public
+    static int MAX_PF = 1000 ;
+    static int MIN_PF = -1000;
+    static int MAX_PARAM = 9;
+    static int MIN_PARAM = 0;
 
     // Factors
     int hf = 0; // History Factor
@@ -16,7 +22,7 @@ public interface Block {
     int pf = 0;
 
     // Pointer to file
-    //File parentFile = new File();
+    File parentFile = null ;
 
     // File number of this block (0 when unused)
     int filenumber = 0;
@@ -24,7 +30,38 @@ public interface Block {
     // IO time
     int IO_time = 0;
 
+//    Block(){
+//
+//    }
+
+    void updateIOtime(){
+        //depends on placement of block so  same file
+        //spatial factor
+    }
+
     // Update Priority Score
-    void updatepf(int lambda, int sigma, int rho, int mu);
+    void updatepf(int lambda, int sigma, int rho, int mu){
+        //ranges of l, sig, rho , mu
+        //ranges of pf check
+        assert lambda <= MAX_PARAM && lambda >= MIN_PARAM ;
+        assert sigma  <= MAX_PARAM && sigma  >= MIN_PARAM ;
+        assert rho    <= MAX_PARAM && rho    >= MIN_PARAM ;
+        assert mu     <= MAX_PARAM && mu     >= MIN_PARAM ;
+
+        int temp = lambda*this.hf - sigma*this.uf + rho*this.sf + mu*this.lf ;
+        if (temp > MAX_PF){
+            this.pf = MAX_PF;
+        }
+        else if (temp < MIN_PF){
+            this.pf = MIN_PF;
+        }
+        else{
+            this.pf = temp;
+        }
+    }
+
+//    int checkRange(int val){
+//
+//    }
 
 }
