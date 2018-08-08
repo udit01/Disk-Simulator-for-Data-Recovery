64f645f06c96109985bfeb826392428cd7356648
udit01 <uditjainiitd@gmail.com>
2018-08-08 10:33:18 +0530

    Added and cleaned block functions

--- src/Block.java
+++ src/Block.java
@@ -8,39 +8,73 @@ public class Block {
     static int MIN_PF = -1000;
     static int MAX_PARAM = 9;
     static int MIN_PARAM = 0;
+    static int MAX_FACTORS = 100000;
+    static int MIN_FACTORS = 0;
 
     // Factors
-    int hf = 0; // History Factor
-    int uf = 1; // Usage Factor
-    int sf = 0; // Spatial Factor
-    int lf = 1; // Linking Factor
+    int hf ; // History Factor
+    int uf ; // Usage Factor
+    int sf ; // Spatial Factor
+    int lf ; // Linking Factor
 
     // Used boolean
-    boolean used = false;
+    boolean used ;
 
     // Priority Score
-    int pf = 0;
+    int pf ;
 
     // Pointer to file
-    File parentFile = null ;
+    File parentFile ;
 
-    // File number of this block (0 when unused)
-    int filenumber = 0;
+    Block(){
+        this.hf = 0; // History Factor
+        this.uf = 1; // Usage Factor
+        this.sf = 0; // Spatial Factor
+        this.lf = 1; // Linking Factor
 
-    // IO time
-    int IO_time = 0;
-
-//    Block(){
-//
-//    }
-
-    void updateIOtime(){
-        //depends on placement of block so  same file
-        //spatial factor
+        this.used = false;
+        this.pf = 0;
+        this.parentFile = null ;
     }
+
+    //allocate this block to a file (UNUSED TO USED TRANSITION)
+    void allocate(File pf, int link_factor){
+        assert this.used == false;
+
+        this.parentFile = pf;
+        this.used = true;
+        this.hf = 1;//reset
+        this.uf = 1;//reset
+
+        this.lf = link_factor;//binaries or non binaries
+    }
+
+    //allocate this block to a file (USED TO UNUSED TRANSITION)
+    void deallocate(){
+        assert this.used == true;
+
+        this.parentFile = null;
+        this.hf = 0;
+        this.used = false;
+
+    }
+
+    void increaseHF(){
+        assert this.used == false;
+
+        this.hf = Math.min(MAX_FACTORS, this.hf + 1);
+    }
+
+    void increaseUF(){
+        assert this.used == true;
+
+        this.uf = Math.min(MAX_FACTORS, this.uf + 1);
+    }
+
 
     // Update Priority Score
     void updatepf(int lambda, int sigma, int rho, int mu){
+
         //ranges of l, sig, rho , mu
         //ranges of pf check
         assert lambda <= MAX_PARAM && lambda >= MIN_PARAM ;
@@ -59,9 +93,5 @@ public class Block {
             this.pf = temp;
         }
     }
-
-//    int checkRange(int val){
-//
-//    }
 
 }
