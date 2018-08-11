f937dd28a656c3c1c6ec8537f1f0cd88af6ce8d7
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2018-08-11 15:05:45 +0530

    fixed bugs in memory

--- src/Memory.java
+++ src/Memory.java
@@ -10,8 +10,8 @@ public class Memory {
     static int MAX_PARAM = 9;
     static int MIN_PARAM = 0;
 
-    int width ;
-    int height ;
+    int width;
+    int height;
 
     double mem_util;
 
@@ -30,7 +30,7 @@ public class Memory {
     ArrayList<File> fileList;
 
 
-    Memory(int w, int h){
+    Memory(int w, int h) {
         this.width = w;
         this.height = h;
         this.blocks = new Block[w][h];
@@ -42,8 +42,8 @@ public class Memory {
         this.rho = 0;
         this.mu = 1;
 
-        for (int i = 0; i < w; i++){
-            for(int j = 0; j < h; j++){
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
                 this.blocks[i][j].i = i;
                 this.blocks[i][j].j = j;
             }
@@ -51,25 +51,25 @@ public class Memory {
 
         this.fileList = new ArrayList<>();
         //put all blocks in unused heap now
-        this.usedBlocks = new HashSet<>(w*h);
+        this.usedBlocks = new HashSet<>(w * h);
 
-        this.unusedBlocks = new PriorityQueue<>(w*h, new BlockComparator());
+        this.unusedBlocks = new PriorityQueue<>(w * h, new BlockComparator());
 //        for ()
-        for (int i = 0; i < w; i++){
-            for(int j = 0; j < h; j++){
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
                 this.unusedBlocks.add(this.blocks[i][j]);
             }
         }
     }
 
-    int createFile(int link_factor, int num_blocks){
+    int createFile(int link_factor, int num_blocks) {
 
         HashSet<Block> block_list = new HashSet<>(num_blocks);
 
-        for(int i = 0; i < num_blocks; i++){
+        for (int i = 0; i < num_blocks; i++) {
             Block b = this.unusedBlocks.poll();
 
-            if(b == null){
+            if (b == null) {
                 throw new java.lang.RuntimeException("Memory Full!");
             }
 
@@ -82,11 +82,11 @@ public class Memory {
 
         this.fileList.add(f);
 
-        for(Block bl : f.blockList){
+        for (Block bl : f.blockList) {
 
             File old_parent = bl.parentFile;
 
-            if (old_parent != null){
+            if (old_parent != null) {
                 old_parent.deleteBlock(bl);
             }
 
@@ -99,11 +99,11 @@ public class Memory {
         return (fileList.size() - 1);
     }
 
-    void deleteFile(int fileIndex){
+    void deleteFile(int fileIndex) {
         File cf = this.fileList.get(fileIndex);
         assert cf != null;
         cf.deleteFile();
-        for(Block b : cf.blockList){
+        for (Block b : cf.blockList) {
             this.usedBlocks.remove(b);
             this.unusedBlocks.add(b);
         }
@@ -112,24 +112,24 @@ public class Memory {
 
     // Update factors of all blocks per transaction
 
-    void readWriteFile(int fileIndex){
+    void readWriteFile(int fileIndex) {
         this.fileList.get(fileIndex).readWriteFile();
         this.refresh();
     }
 
-    void updateSF(){
+    void updateSF() {
         // neightbouring blocks's pf's avg in sf of this block
         ArrayList<Pair<Integer, Integer>> directions = new ArrayList<Pair<Integer, Integer>>(8);
 
-        directions.add(new Pair<>(-1,-1));
+        directions.add(new Pair<>(-1, -1));
         directions.add(new Pair<>(-1, 0));
         directions.add(new Pair<>(-1, 1));
 
-        directions.add(new Pair<>( 0,-1));
+        directions.add(new Pair<>(0, -1));
 //        directions.add(new Pair<>( 0, 0));
-        directions.add(new Pair<>( 0, 1));
+        directions.add(new Pair<>(0, 1));
 
-        directions.add(new Pair<>(1,-1));
+        directions.add(new Pair<>(1, -1));
         directions.add(new Pair<>(1, 0));
         directions.add(new Pair<>(1, 1));
 
@@ -139,39 +139,39 @@ public class Memory {
         int newi = 0;
         int newj = 0;
 
-        for (int i = 0; i < this.width; i++){
-            for(int j = 0; j < this.height; j++){
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
 
                 sum = 0.0;
                 count = 0;
 
-                for (int t = 0; t < directions.size(); t++ ){
+                for (int t = 0; t < directions.size(); t++) {
                     newi = i + directions.get(t).getKey();
                     newj = i + directions.get(t).getValue();
 
-                    if (newi >= 0 && newi < this.width && newj >= 0 && newj < this.height ){
+                    if (newi >= 0 && newi < this.width && newj >= 0 && newj < this.height) {
                         sum += this.blocks[newi][newj].pf;
                         count++;
                     }
                 }
 
-                this.blocks[i][j].setSF((int)(sum/count));
+                this.blocks[i][j].setSF((int) (sum / count));
 
             }
         }
 
     }
 
-    void refresh(){
+    void refresh() {
         this.updateSF();
 
-        PriorityQueue<Block> newHeap = new PriorityQueue<>(this.width*this.height, new BlockComparator());
+        PriorityQueue<Block> newHeap = new PriorityQueue<>(this.width * this.height, new BlockComparator());
 
-        for (int i = 0; i < this.width; i++){
-            for(int j = 0; j < this.height; j++){
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
                 Block b = this.blocks[i][j];
                 b.updatepf(this.lambda, this.sigma, this.rho, this.mu);
-                if (b.used == false){
+                if (b.used == false) {
                     newHeap.add(this.blocks[i][j]);
                 }
             }
@@ -179,19 +179,19 @@ public class Memory {
 
         this.unusedBlocks = newHeap;
 
-        this.mem_util = ((double)(this.usedBlocks.size()))/(this.width*this.height);
+        this.mem_util = ((double) (this.usedBlocks.size())) / (this.width * this.height);
 
     }
 
-    void updateParams(int lambda, int sigma, int rho, int mu){
-        assert (lambda <= MAX_PARAM) && (lambda >= MIN_PARAM );
-        assert (sigma  <= MAX_PARAM) && (sigma  >= MIN_PARAM );
-        assert (rho    <= MAX_PARAM) && (rho    >= MIN_PARAM );
-        assert (mu     <= MAX_PARAM) && (mu     >= MIN_PARAM );
+    void updateParams(int lambda, int sigma, int rho, int mu) {
+        assert (lambda <= MAX_PARAM) && (lambda >= MIN_PARAM);
+        assert (sigma <= MAX_PARAM) && (sigma >= MIN_PARAM);
+        assert (rho <= MAX_PARAM) && (rho >= MIN_PARAM);
+        assert (mu <= MAX_PARAM) && (mu >= MIN_PARAM);
 
         this.lambda = lambda;
         this.sigma = sigma;
-        this.rho  = rho;
+        this.rho = rho;
         this.mu = mu;
 
     }
@@ -202,6 +202,8 @@ public class Memory {
 //    }
 
     // Memory Usage percentage
-    double memUsage(){
+    double memUsage() {
         return mem_util;
     }
+
+}
